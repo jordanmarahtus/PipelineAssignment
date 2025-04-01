6d7619a35dd6a7f4106c18781e260527d4407ffb
Jordan <A00326246@student.tus.ie>
2025-04-01 19:24:33 +0100

    update

--- bookservice/src/main/java/com/library/bookservice/BookserviceApplication.java
+++ bookservice/src/main/java/com/library/bookservice/BookserviceApplication.java
@@ -2,10 +2,8 @@ package com.library.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BookserviceApplication {
 
 	public static void main(String[] args) {
